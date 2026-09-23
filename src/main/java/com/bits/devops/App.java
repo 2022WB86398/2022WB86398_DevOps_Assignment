1733c0cf5ce0e4c32d5ff9cf710570451bccb4a9
Yuvraj Yadav <2022wb86398@wilp.bits-pilani.ac.in>
2026-09-24 00:49:23 +0530

    Modify source code to test Jenkins Poll SCM

--- src/main/java/com/bits/devops/App.java
+++ src/main/java/com/bits/devops/App.java
@@ -1,6 +1,11 @@
 package com.bits.devops;
+
 public class App {
-public static void main(String[] args) {
-System.out.println("Continuous Integration using Git, Maven and Jenkins");
+    public static void main(String[] args) {
+        System.out.println("DevOps Continuous Integration Project");
+        System.out.println("BITS ID: 2022WB86398");
+        System.out.println("Source code modified for Jenkins Poll SCM");
+        System.out.println("Continuous Integration completed successfully");
+    }
 }
-}
+
